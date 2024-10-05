01fa4e4262c1d36710113f706041df1acf0acdb4
shivam <shvam12340987@gmail.com>
2024-10-05 09:27:34 +0530

    changes in wishlisht

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -35,167 +35,190 @@ const Abril = Abril_Fatface({
   display: "swap",
 });
 
-const Header = ({ setisWishlistOpen }) => {
+const Header = () => {
   const [isFousOnSearch, setisFousOnSearch] = useState(false);
   const [isSideBarOpen, setisSideBarOpen] = useState(false);
 
+  const [isWishlistOpen, setisWishlistOpen] = useState(false);
+
   return (
     <>
-      {/* mobile animated slider of side bar */}
-      <motion.div
-        initial={{ x: -500 }}
-        animate={{ x: isSideBarOpen ? 0 : -500 }}
-        transition={{ duration: 0.3 }}
-        className={`fixed w-full h-full z-50   bg-white top-0 max-w-[500px] left-0`}
-      >
-        <RxCross1
-          size={30}
-          onClick={() => setisSideBarOpen(false)}
-          className="ml-auto m-3 cursor-pointer"
-        />
+      <div className="w-full overflow-x-hidden">
+        <motion.div
+          initial={{ x: 1000, opacity: 0 }}
+          animate={{
+            x: isWishlistOpen ? 0 : 1000,
+            opacity: isWishlistOpen ? 1 : 0,
+          }}
+          transition={{ duration: 0.4 }}
+          className="h-full w-full 500px:w-[500px] z-50  bg-white fixed top-0 right-0"
+        >
+          <RxCross1
+            onClick={() => setisWishlistOpen(false)}
+            size={30}
+            className="m-4 cursor-pointer hover:scale-105 "
+          />
+        </motion.div>
 
-        {/* user profile  */}
-        <div className="flex gap-3 items-center mx-4  border-b-2 pb-5">
-          <div className="w-20 h-20  relative rounded-full">
-            <Image
-              fill={true}
-              alt="profile"
-              className="bg-cyan-200 rounded-full"
-              // src={"j"}
-            ></Image>
-          </div>
-          <div>
-            <p className="text-lg font-semibold ">Shivam</p>
-            <p className="text-sm text-gray-500">shivam@gmail.com</p>
-          </div>
-        </div>
+        {/* mobile animated slider of side bar */}
+        <motion.div
+          initial={{ x: -500 }}
+          animate={{ x: isSideBarOpen ? 0 : -500 }}
+          transition={{ duration: 0.3 }}
+          className={`fixed w-full h-full z-50   bg-white top-0 max-w-[500px] left-0`}
+        >
+          <RxCross1
+            size={30}
+            onClick={() => setisSideBarOpen(false)}
+            className="ml-auto m-3 cursor-pointer"
+          />
 
-        {/* navigation and functionality of side bar */}
-
-        <div className="flex flex-col p-2 gap-1 px-3  ">
-          {navigations?.map((i) => (
-            <div
-              key={i}
-              className="flex items-center justify-between p-3 rounded-xl cursor-pointer hover:bg-gray-200"
-            >
-              <div className="flex gap-2 items-center  ">
-                {i?.icon}
-                <p className="font-semibold">{i?.title}</p>
-              </div>
-
-              <FaChevronRight />
-            </div>
-          ))}
-        </div>
-      </motion.div>
-      <div className="block h-12  800px:h-16">
-        {/* desktop header  */}
-        <div className="max-800px:hidden fixed z-40 bg-white">
-          <div
-            className={
-              "flex items-center    justify-between   border-b-[1px] border-gray-300 w-[100vw] py-2 h-20"
-            }
-          >
-            <Link href="/" className="flex items-center text-[160%] font-bold">
+          {/* user profile  */}
+          <div className="flex gap-3 items-center mx-4  border-b-2 pb-5">
+            <div className="w-20 h-20  relative rounded-full">
               <Image
-                height={60}
-                width={60}
-                alt="logo"
-                src={companyLogo}
-                className="ml-4 1200px:ml-10"
-              />
-              <p className={Abril.className}>Urban Cart</p>
-            </Link>
-
-            <div className="flex max-1000px:hidden gap-5 ">
-              <Link
-                href="/products?c=MEN"
-                className="border-b-2 border-b-white hover:border-b-black cursor-pointer font-semibold text-[16px] "
-              >
-                MEN
-              </Link>
-              <Link
-                href="/products?c=WOMEN"
-                className="border-b-2 border-b-white hover:border-b-black cursor-pointer font-semibold text-[16px] "
-              >
-                WOMEN
-              </Link>
-              <Link
-                href="/products?c=KIDS"
-                className="border-b-2 border-b-white hover:border-b-black cursor-pointer font-semibold text-[16px] "
-              >
-                KIDS
-              </Link>
+                fill={true}
+                alt="profile"
+                className="bg-cyan-200 rounded-full"
+                // src={"j"}
+              ></Image>
             </div>
+            <div>
+              <p className="text-lg font-semibold ">Shivam</p>
+              <p className="text-sm text-gray-500">shivam@gmail.com</p>
+            </div>
+          </div>
 
-            <div className="flex items-center ">
+          {/* navigation and functionality of side bar */}
+
+          <div className="flex flex-col p-2 gap-1 px-3  ">
+            {navigations?.map((i) => (
               <div
-                className={`h-9 w-56 bg-gray-200 flex items-center border-2  p-2 ${
-                  isFousOnSearch && "rounded-xl border-blue-400 "
-                }  gap-2`}
+                key={i}
+                className="flex items-center justify-between p-3 rounded-xl cursor-pointer hover:bg-gray-200"
               >
-                <CiSearch size={32} className="text-gray-500" />
-                <input
-                  type="text"
-                  className="w-full bg-gray-200 outline-none h-full"
-                  placeholder="Search"
-                  onFocus={() => setisFousOnSearch(true)}
-                  onBlur={() => setisFousOnSearch(false)}
-                />
-              </div>
-              <div className="ml-[3vw] mr-[10vw] gap-3 flex items-center">
-                <div className="bg-black border-2 border-black transition-all duration-400 hover:scale-105 text-white px-5 py-1 rounded-lg text-sm 1000px:text-lg font-semibold cursor-pointer hover:bg-white hover:text-black ">
-                  Login
+                <div className="flex gap-2 items-center  ">
+                  {i?.icon}
+                  <p className="font-semibold">{i?.title}</p>
                 </div>
 
-                <div
-                  onClick={() => setisWishlistOpen(true)}
-                  className="flex flex-col items-center ml-[3vw] text-red-500 cursor-pointer hover hover:text-gray-500"
-                >
-                  <CiHeart size={30} />
-                  <p className="text-sm font-medium">Wishlist</p>
-                </div>
+                <FaChevronRight />
+              </div>
+            ))}
+          </div>
+        </motion.div>
+        <div className="block h-12  800px:h-16">
+          {/* desktop header  */}
+          <div className="max-800px:hidden fixed z-40 bg-white">
+            <div
+              className={
+                "flex items-center    justify-between   border-b-[1px] border-gray-300 w-[100vw] py-2 h-20"
+              }
+            >
+              <Link
+                href="/"
+                className="flex items-center text-[160%] font-bold"
+              >
+                <Image
+                  height={60}
+                  width={60}
+                  alt="logo"
+                  src={companyLogo}
+                  className="ml-4 1200px:ml-10"
+                />
+                <p className={Abril.className}>Urban Cart</p>
+              </Link>
+
+              <div className="flex max-1000px:hidden gap-5 ">
                 <Link
-                  href={"/cart"}
-                  className="flex flex-col items-center ml-[1vw] text-black cursor-pointer hover hover:text-gray-500"
+                  href="/products?c=MEN"
+                  className="border-b-2 border-b-white hover:border-b-black cursor-pointer font-semibold text-[16px] "
                 >
-                  <GiShoppingBag size={30} />
-                  <p className="text-sm font-medium">Cart</p>
+                  MEN
+                </Link>
+                <Link
+                  href="/products?c=WOMEN"
+                  className="border-b-2 border-b-white hover:border-b-black cursor-pointer font-semibold text-[16px] "
+                >
+                  WOMEN
+                </Link>
+                <Link
+                  href="/products?c=KIDS"
+                  className="border-b-2 border-b-white hover:border-b-black cursor-pointer font-semibold text-[16px] "
+                >
+                  KIDS
                 </Link>
               </div>
+
+              <div className="flex items-center ">
+                <div
+                  className={`h-9 w-56 bg-gray-200 flex items-center border-2  p-2 ${
+                    isFousOnSearch && "rounded-xl border-blue-400 "
+                  }  gap-2`}
+                >
+                  <CiSearch size={32} className="text-gray-500" />
+                  <input
+                    type="text"
+                    className="w-full bg-gray-200 outline-none h-full"
+                    placeholder="Search"
+                    onFocus={() => setisFousOnSearch(true)}
+                    onBlur={() => setisFousOnSearch(false)}
+                  />
+                </div>
+                <div className="ml-[3vw] mr-[10vw] gap-3 flex items-center">
+                  <div className="bg-black border-2 border-black transition-all duration-400 hover:scale-105 text-white px-5 py-1 rounded-lg text-sm 1000px:text-lg font-semibold cursor-pointer hover:bg-white hover:text-black ">
+                    Login
+                  </div>
+
+                  <div
+                    onClick={() => setisWishlistOpen(true)}
+                    className="flex flex-col items-center ml-[3vw] text-red-500 cursor-pointer hover hover:text-gray-500"
+                  >
+                    <CiHeart size={30} />
+                    <p className="text-sm font-medium">Wishlist</p>
+                  </div>
+                  <Link
+                    href={"/cart"}
+                    className="flex flex-col items-center ml-[1vw] text-black cursor-pointer hover hover:text-gray-500"
+                  >
+                    <GiShoppingBag size={30} />
+                    <p className="text-sm font-medium">Cart</p>
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* mobile header  */}
-        <div className="fixed w-full 800px:hidden bg-white z-40">
-          <div className=" w-full  z-30 items-center   text-gray-500 p-3 flex shadow-md justify-between   ">
-            <IoReorderThreeOutline
-              onClick={() => setisSideBarOpen(true)}
-              className="text-[40px] cursor-pointer hover:text-gray-300"
-            />
+          {/* mobile header  */}
+          <div className="fixed w-full 800px:hidden bg-white z-40">
+            <div className=" w-full  z-30 items-center   text-gray-500 p-3 flex shadow-md justify-between   ">
+              <IoReorderThreeOutline
+                onClick={() => setisSideBarOpen(true)}
+                className="text-[40px] cursor-pointer hover:text-gray-300"
+              />
 
-            <div className="flex gap-3 items-center">
-              <IoSearchOutline className="text-[25px]" />
-              <Link href="/cart">
-                <IoBagHandleSharp className="text-[25px]" />
-              </Link>
-              <FaRegHeart className="text-[23px] text-gray-500" />
+              <div className="flex gap-3 items-center">
+                <IoSearchOutline className="text-[25px]" />
+                <Link href="/cart">
+                  <IoBagHandleSharp className="text-[25px]" />
+                </Link>
+                <FaRegHeart className="text-[23px] text-gray-500" />
+              </div>
             </div>
-          </div>
 
-          <div className="flex overflow-x-scroll hide-scrollbar p-3 gap-4  bg-white">
-            {cat.map((i) => (
-              <Link
-                href={`/products?c=${i}`}
-                className={
-                  "bg-gray-200 shadow-xl  font-semibold text-black px-4 800px:px-6 py-1 800px:py-2 rounded-xl hover:bg-black hover:text-white text-sm 800px:text-lg transition-all duration-150"
-                }
-                key={i}
-              >
-                {i}
-              </Link>
-            ))}
+            <div className="flex overflow-x-scroll hide-scrollbar p-3 gap-4  bg-white">
+              {cat.map((i) => (
+                <Link
+                  href={`/products?c=${i}`}
+                  className={
+                    "bg-gray-200 shadow-xl  font-semibold text-black px-4 800px:px-6 py-1 800px:py-2 rounded-xl hover:bg-black hover:text-white text-sm 800px:text-lg transition-all duration-150"
+                  }
+                  key={i}
+                >
+                  {i}
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -24,92 +24,29 @@ const roboto = Quicksand({
 });
 
 const Home = () => {
-  const [isWishlistOpen, setisWishlistOpen] = useState(false);
-
   return (
-    <div className="w-full overflow-x-hidden">
-      {/* wishlist */}
-      <motion.div
-        initial={{ x: 1000, opacity: 0 }}
-        animate={{
-          x: isWishlistOpen ? 0 : 1000,
-          opacity: isWishlistOpen ? 1 : 0,
-        }}
-        transition={{ duration: 0.4 }}
-        className="h-full w-full 500px:w-[500px] z-50  bg-white fixed top-0 right-0"
-      >
-        <RxCross1
-          onClick={() => setisWishlistOpen(false)}
-          size={30}
-          className="m-4 cursor-pointer hover:scale-105 "
-        />
-      </motion.div>
+    <div className={roboto.className}>
+      <Head>
+        <title>Urban Cart</title>
+        <link rel="icon" href="/favicon.ico" /> {/* Updated favicon path */}
+      </Head>
+      <Header />
+      {/* <Banner /> */}
 
-      <div className={roboto.className}>
-        <Head>
-          <title>Urban Cart</title>
-          <link rel="icon" href="/favicon.ico" /> {/* Updated favicon path */}
-        </Head>
-        <Header setisWishlistOpen={setisWishlistOpen} />
-        {/* <Banner /> */}
+      {/* banners slae  */}
 
-        {/* banners slae  */}
-
-        {/* tending products  */}
-        <div className="max-800px:pt-12  ">
-          <div className="flex justify-center bg-gray-300">
-            <div className="p-3 w-full h-52 550px:h-64  800px:h-80 1000px:w-[1300px] 1000px:h-[600px] relative">
-              <Image fill={true} alt="sale" src={saleImage} />
-            </div>
-          </div>
-          <p className="text-xl 800px:text-2xl font-bold text-gray-600 px-3 mt-4 mb-2 800px:p-7 animate-pulse">
-            Trending
-          </p>
-          <div className="flex mt-2 flex-wrap justify-center w-full">
-            {dummyProducts.slice(0, 6)?.map((item, i) => (
-              <div
-                key={i}
-                className="h-64 bg-gray-200 border-2 border-white w-[48%] 500px:h-72 600px:h-68 600px:w-[32%] 800px:w-[37%] 900px:w-[30%] 900px:h-72 1050px:w-[32%] 1050px:h-80 1300px:h-[400px] 1500px:w-[25%]  1750px:h-[420] 1950px:h-[440] 2000px:h-[460px]"
-              >
-                <div className="w-full h-[70%] 800px:h-[80%] relative">
-                  <Image
-                    lazy="true"
-                    alt="Product image"
-                    src={item?.images[0]}
-                    fill="true"
-                  />
-                </div>
-                <div className="px-2">
-                  <p className="text-sm font-semibold text-gray-500">
-                    {item?.title}
-                  </p>
-                  <div className="flex items-center justify-between">
-                    <div className="flex gap-2">
-                      <p className="text-sm 800px:text-lg font-semibold">
-                        ${item?.price}
-                      </p>
-                      <p className="text-sm font-semibold line-through">
-                        ${Math.floor(item?.price * (1 + Math.random()))}
-                      </p>
-                    </div>
-                    <p className="bg-green-500 px-2 py-1 text-sm 1000px:text-lg rounded-xl text-white animate-bounce">
-                      13% off
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
+      {/* tending products  */}
+      <div className="max-800px:pt-12  ">
+        <div className="flex justify-center bg-gray-300">
+          <div className="p-3 w-full h-52 550px:h-64  800px:h-80 1000px:w-[1300px] 1000px:h-[600px] relative">
+            <Image fill={true} alt="sale" src={saleImage} />
           </div>
         </div>
-
-        <SaleBanner />
-
-        <LowwersCarousel />
-        <p className="text-xl 800px:text-2xl font-bold text-gray-600 px-3 mb-2 800px:p-7 pt-1 animate-pulse">
-          New Arrival
+        <p className="text-xl 800px:text-2xl font-bold text-gray-600 px-3 mt-4 mb-2 800px:p-7 animate-pulse">
+          Trending
         </p>
         <div className="flex mt-2 flex-wrap justify-center w-full">
-          {dummyProducts.slice(6, 12)?.map((item, i) => (
+          {dummyProducts.slice(0, 6)?.map((item, i) => (
             <div
               key={i}
               className="h-64 bg-gray-200 border-2 border-white w-[48%] 500px:h-72 600px:h-68 600px:w-[32%] 800px:w-[37%] 900px:w-[30%] 900px:h-72 1050px:w-[32%] 1050px:h-80 1300px:h-[400px] 1500px:w-[25%]  1750px:h-[420] 1950px:h-[440] 2000px:h-[460px]"
@@ -143,9 +80,51 @@ const Home = () => {
             </div>
           ))}
         </div>
-        {/* <Footer /> */}
-        <Footer />
       </div>
+
+      <SaleBanner />
+
+      <LowwersCarousel />
+      <p className="text-xl 800px:text-2xl font-bold text-gray-600 px-3 mb-2 800px:p-7 pt-1 animate-pulse">
+        New Arrival
+      </p>
+      <div className="flex mt-2 flex-wrap justify-center w-full">
+        {dummyProducts.slice(6, 12)?.map((item, i) => (
+          <div
+            key={i}
+            className="h-64 bg-gray-200 border-2 border-white w-[48%] 500px:h-72 600px:h-68 600px:w-[32%] 800px:w-[37%] 900px:w-[30%] 900px:h-72 1050px:w-[32%] 1050px:h-80 1300px:h-[400px] 1500px:w-[25%]  1750px:h-[420] 1950px:h-[440] 2000px:h-[460px]"
+          >
+            <div className="w-full h-[70%] 800px:h-[80%] relative">
+              <Image
+                lazy="true"
+                alt="Product image"
+                src={item?.images[0]}
+                fill="true"
+              />
+            </div>
+            <div className="px-2">
+              <p className="text-sm font-semibold text-gray-500">
+                {item?.title}
+              </p>
+              <div className="flex items-center justify-between">
+                <div className="flex gap-2">
+                  <p className="text-sm 800px:text-lg font-semibold">
+                    ${item?.price}
+                  </p>
+                  <p className="text-sm font-semibold line-through">
+                    ${Math.floor(item?.price * (1 + Math.random()))}
+                  </p>
+                </div>
+                <p className="bg-green-500 px-2 py-1 text-sm 1000px:text-lg rounded-xl text-white animate-bounce">
+                  13% off
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
