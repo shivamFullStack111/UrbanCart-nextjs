481560ff63aba2cb54abbc0251645a4ba13a4bb3
shivam <shvam12340987@gmail.com>
2024-10-08 17:38:38 +0530

    debugging

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -3,14 +3,7 @@ import User from "@/models/User";
 
 export async function GET(request) {
   await dbConnect();
-  const newUser = new User({
-    name: "John Doe",
-    email: "john@gmail.com",
-    phoneNumber: 3487845873598,
-    password: "4gbfejhvbjebf",
-  });
 
-  await newUser.save();
   return new Response(
     JSON.stringify({ success: true, message: "login success" })
   );
