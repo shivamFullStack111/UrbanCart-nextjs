ae3462450727af26c1cad50c73309d69120da855
shivam <shvam12340987@gmail.com>
2024-10-11 20:19:40 +0530

    create product working

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "images.meesho.com",
-      "images.bewakoof.com",
-      "cdn.dummyjson.com",
-      "plus.unsplash.com",
-      "encrypted-tbn1.gstatic.com",
-    ], // Add your external image hostnames here
-    // Optionally, configure other image settings like deviceSizes, imageSizes, etc.
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
