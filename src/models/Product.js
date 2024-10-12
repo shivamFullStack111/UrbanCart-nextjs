e8e501bc5848edd1ce34630fa69c3ebc1e00cebe
shivam <shvam12340987@gmail.com>
2024-10-12 14:31:20 +0530

    get product test

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,38 +1,35 @@
-// models/Product.js
-
-import mongoose from "mongoose";
+const { default: mongoose } = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true }, // Corrected typo and added validation
-    gender: { type: String, required: true },
-    category: { type: String, required: true, index: true }, // Indexed for faster queries
-    brand: { type: String, required: true },
-    clothType: { type: String, required: true },
-    stockKeepingUnit: { type: Number, unique: true, required: true }, // Unique SKU
-    mrpPrice: { type: Number, required: true },
-    sellingPrice: { type: Number, required: true },
-    stock: { type: Number, required: true, min: 0 }, // Prevent negative stock
-    totalSale: { type: Number, default: 0, min: 0 }, // Prevent negative stock
-    description: { type: String, required: true },
-    images: { type: [String], required: true }, // Array of image URLs
-    totalRating: { type: Number, default: 0 },
-    // Specification
-    colors: { type: [String], default: [] },
-    sizes: { type: [String], default: [] },
-    material: { type: String, required: true },
-    clothPattern: { type: String },
-    fitType: { type: String },
-    sleeveType: { type: String },
-    neckType: { type: String },
-    heelHeight: { type: Number, default: 0 }, // Optional for non-shoe products
-    soleMaterial: { type: String, default: "" }, // Optional
+    tittle: String,
+    gender: String,
+    category: String,
+    brand: String,
+    clothType: String,
+    stockKeepingUnit: Number,
+    mrpPrice: Number,
+    sellingPrice: Number,
+    stock: Number,
+    description: String,
+    images: Array,
+    totalRating: Number,
+    totalSale: Number,
+    // specification
+    colors: Array,
+    sizes: Array,
+    material: String,
+    clothPattern: String,
+    fitType: String,
+    sleeveType: String,
+    neckType: String,
+    heelHeight: Number,
+    soleMaterial: String,
   },
   { timestamps: true }
-); // Automatically manage createdAt and updatedAt fields
+);
 
-// Use singular model name for clarity
 const Product =
-  mongoose.models.Product || mongoose.model("Product", productSchema);
+  mongoose.models.Products || mongoose.model("Products", productSchema);
 
 export default Product;
