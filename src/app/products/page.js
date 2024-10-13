16c0b0c1e6b6d8146d01f371e75ab0ef8459c43e
shivam <shvam12340987@gmail.com>
2024-10-13 17:09:19 +0530

    dynamic product page

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -109,36 +109,58 @@ export default Products;
 
 const ProductsContent = ({ products }) => {
   return (
-    <div className="w-full h-full  p-2 gap-1  grid  grid-cols-1 300px:grid-cols-2 550px:grid-cols-3 1200px:grid-cols-4 1800px:grid-cols-5 ">
-      {products?.map((item, i) => (
-        <div
-          key={i}
-          className="w-full h-[110vw] 300px:h-[64vw] 550px:h-[40vw] 800px:h-[30vw] 1200px:h-[27vw] 1800px:h-[22vw] "
-        >
-          <div className="w-full h-[80%] relative">
-            <Image fill={true} src={item?.images[0]} className=""></Image>
-          </div>
-
-          <div className="px-2">
-            <p className="text-lg font-semibold text-gray-700">
-              {item?.tittle}
-            </p>
-            <div className="flex items-center justify-between">
-              <div className="flex gap-2">
-                <p className="text-sm 800px:text-lg font-semibold">
-                  ${item?.sellingPrice}
-                </p>
-                <p className="text-sm font-semibold line-through">
-                  {item?.mrpPrice}
-                </p>
+    <div className="w-full h-full p-2 gap-1 grid grid-cols-1 300px:grid-cols-2 550px:grid-cols-3 1200px:grid-cols-4 1800px:grid-cols-5">
+      {products
+        ? products.map((item, i) => (
+            <div
+              key={i}
+              className="w-full h-[110vw] 300px:h-[64vw] 550px:h-[40vw] 800px:h-[30vw] 1200px:h-[27vw] 1800px:h-[22vw]"
+            >
+              <div className="w-full h-[80%] relative">
+                <Image fill={true} src={item?.images[0]} className="" />
               </div>
-              <p className="bg-green-500 px-2 py-[2px] text-[10px] 1000px:text-sm rounded-sm text-white ">
-                13% off
-              </p>
+
+              <div className="px-2">
+                <p className="text-lg font-semibold text-gray-700">
+                  {item?.tittle}
+                </p>
+                <div className="flex items-center justify-between">
+                  <div className="flex gap-2">
+                    <p className="text-sm 800px:text-lg font-semibold">
+                      ${item?.sellingPrice}
+                    </p>
+                    <p className="text-sm font-semibold line-through">
+                      {item?.mrpPrice}
+                    </p>
+                  </div>
+                  <p className="bg-green-500 px-2 py-[2px] text-[10px] 1000px:text-sm rounded-sm text-white ">
+                    13% off
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      ))}
+          ))
+        : // Skeleton Loader
+          [...Array(20)].map((_, i) => (
+            <div
+              key={i}
+              className="w-full h-[110vw] 300px:h-[64vw] bg-gray-400 animate-pulse 550px:h-[40vw] 800px:h-[30vw] 1200px:h-[27vw] 1800px:h-[22vw]"
+            >
+              <div className="w-full h-[80%] bg-gray-300"></div>
+              <div className="px-2">
+                <p className="h-4 bg-gray-300 animate-pulse rounded-md"></p>
+                <div className="flex items-center justify-between mt-2">
+                  <div className="flex gap-2">
+                    <p className="h-4 bg-gray-300 animate-pulse rounded-md w-12"></p>
+                    <p className="h-4 bg-gray-300 animate-pulse rounded-md w-12"></p>
+                  </div>
+                  <p className="bg-green-500 px-2 py-[2px] text-[10px] rounded-sm text-white animate-pulse">
+                    &nbsp;
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
     </div>
   );
 };
