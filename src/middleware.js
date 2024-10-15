85337e2dede2c3b71261e09bcd5ba9245cf2f9a6
shivam <shvam12340987@gmail.com>
2024-10-15 17:44:32 +0530

    make some better to address component

--- src/middleware.js
+++ src/middleware.js
@@ -7,48 +7,62 @@ export async function middleware(request) {
   const token = request.cookies.get("token_urbancart")?.value;
 
   // Check if token exists
-  if (token) {
-    try {
-      const res = await axios.get(
-        `${request.nextUrl.origin}/api/isauthenticated`,
-        {
-          headers: { Authorization: token },
+  // if (token) {
+  //   try {
+  //     const res = await axios.get(
+  //       `${request.nextUrl.origin}/api/isauthenticated`,
+  //       {
+  //         headers: { Authorization: token },
 
-          timeout: 10000, // 10 seconds
-        }
+  //         timeout: 10000, // 10 seconds
+  //       }
+  //     );
+
+  //     console.log(res.data.success);
+  //     console.log(currentPath);
+
+  //     // Redirect if user is authenticated and trying to access login/register
+  //     if (currentPath === "/login" || currentPath === "/register") {
+  //       if (res.data?.success) {
+  //         console.log("User already authenticated, redirecting to home.");
+  //         return NextResponse.redirect(new URL("/", request.url));
+  //       }
+  //     } else {
+  //       // Redirect to login if not authenticated and trying to access protected route
+  //       if (!res.data.success) {
+  //         console.log("User not authenticated, redirecting to login.");
+  //         return NextResponse.redirect(new URL("/login", request.url));
+  //       }
+  //     }
+  //   } catch (error) {
+  //     alert("Error in middleware:", error.message);
+  //     // Optionally redirect to a fallback page if the API call fails
+  //     return NextResponse.redirect(new URL("/error", request.url));
+  //   }
+  // } else {
+  //   // If no token, redirect to login if trying to access protected routes
+  //   if (currentPath !== "/login" && currentPath !== "/register") {
+  //     console.log("No token, redirecting to login.");
+  //     return NextResponse.redirect(new URL("/login", request.url));
+  //   }
+  // }
+  console.log(currentPath, "orihginjrf :-", request.nextUrl.origin, token);
+  if (currentPath === "/login" || currentPath === "/register") {
+    if (token) {
+      return NextResponse.redirect(
+        new URL(`${request.nextUrl.origin}/`, request.url)
       );
-
-      console.log(res.data.success);
-      console.log(currentPath);
-
-      // Redirect if user is authenticated and trying to access login/register
-      if (currentPath === "/login" || currentPath === "/register") {
-        if (res.data?.success) {
-          console.log("User already authenticated, redirecting to home.");
-          return NextResponse.redirect(new URL("/", request.url));
-        }
-      } else {
-        // Redirect to login if not authenticated and trying to access protected route
-        if (!res.data.success) {
-          console.log("User not authenticated, redirecting to login.");
-          return NextResponse.redirect(new URL("/login", request.url));
-        }
-      }
-    } catch (error) {
-      alert("Error in middleware:", error.message);
-      // Optionally redirect to a fallback page if the API call fails
-      return NextResponse.redirect(new URL("/error", request.url));
     }
   } else {
-    // If no token, redirect to login if trying to access protected routes
-    if (currentPath !== "/login" && currentPath !== "/register") {
-      console.log("No token, redirecting to login.");
-      return NextResponse.redirect(new URL("/login", request.url));
+    if (!token) {
+      return NextResponse.redirect(
+        new URL(`${request.nextUrl.origin}/login`, request.url)
+      );
     }
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/about/:path*", "/login", "/register", "/cart"],
+  matcher: ["/cart", "/login", "/register"],
 };
